a9cf2e58481c7ae5dbaf02f243a9bbd37a3a263c
Seyed Mohammad Hadi Tabatabaei <hadi.tabatabaei.aut@gmail.com>
2019-12-15 00:28:55 +0330

    test some http and file modules

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Net.Http;
+using System.IO;
+namespace csharp_tutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try {
+                HttpClient client = new HttpClient();
+                string body = client.GetStringAsync("http://www.google.com").Result;
+                FileInfo fileInfo = new FileInfo("google.html");
+                StreamWriter sr = new StreamWriter(fileInfo.Create());
+                sr.WriteLine(body);
+                sr.Close();
+                client.Dispose();
+            } catch (HttpRequestException e) {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
